80d0f7050d730a723c7292bf90151365c88e2882
Bryan Ames <bryan@gantep.org>
2021-07-24 13:46:14 -0700

    updated preonic map with miryoku style layout

--- keyboards/preonic/keymaps/amesb/config.h
+++ keyboards/preonic/keymaps/amesb/config.h
@@ -4,13 +4,15 @@
     #define STARTUP_SONG SONG(PREONIC_SOUND)
     // #define STARTUP_SONG SONG(NO_SOUND)
 
+/*
     #define DEFAULT_LAYER_SONGS { SONG(QWERTY_SOUND), \
                                   SONG(COLEMAK_SOUND), \
                                   SONG(DVORAK_SOUND) \
                                 }
+*/
 #endif
 
-#define MUSIC_MASK (keycode != KC_NO)
+//#define MUSIC_MASK (keycode != KC_NO)
 
 /*
  * MIDI options
@@ -20,7 +22,7 @@
    - MIDI notes can be sent when in Music mode is on
 */
 
-#define MIDI_BASIC
+//#define MIDI_BASIC
 
 /* enable advanced MIDI features:
    - MIDI notes can be added to the keymap
@@ -35,5 +37,42 @@
 
 
 // One Shot Modifier (sticky keys) settings
-#define ONESHOT_TAP_TOGGLE 3
-#define ONESHOT_TIMEOUT 1000
+//#define ONESHOT_TAP_TOGGLE 3
+//#define ONESHOT_TIMEOUT 1000
+
+
+
+// miryoku features
+
+// default but used in macros
+#undef TAPPING_TERM
+#define TAPPING_TERM 200
+
+// Prevent normal rollover on alphas from accidentally triggering mods.
+#define IGNORE_MOD_TAP_INTERRUPT
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define TAPPING_FORCE_HOLD
+
+// Auto Shift
+#define NO_AUTO_SHIFT_ALPHA
+#define AUTO_SHIFT_TIMEOUT TAPPING_TERM
+#define AUTO_SHIFT_NO_SETUP
+
+// Recommended for heavy chording.
+#define QMK_KEYS_PER_SCAN 4
+
+// Mouse key speed and acceleration.
+#undef MOUSEKEY_DELAY
+#define MOUSEKEY_DELAY          0
+#undef MOUSEKEY_INTERVAL
+#define MOUSEKEY_INTERVAL       16
+#undef MOUSEKEY_WHEEL_DELAY
+#define MOUSEKEY_WHEEL_DELAY    0
+#undef MOUSEKEY_MAX_SPEED
+#define MOUSEKEY_MAX_SPEED      6
+#undef MOUSEKEY_TIME_TO_MAX
+#define MOUSEKEY_TIME_TO_MAX    64
+
+// additional rgb options
+#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
--- keyboards/preonic/keymaps/amesb/rules.mk
+++ keyboards/preonic/keymaps/amesb/rules.mk
@@ -1,1 +1,7 @@
-SRC += muse.c
+MOUSEKEY_ENABLE = yes # Mouse keys
+EXTRAKEY_ENABLE = yes # Audio control and System control
+AUTO_SHIFT_ENABLE = no # Auto Shift
+RGBLIGHT_ENABLE = no # disable basic rgb features
+RGB_MATRIX_ENABLE = yes # enable rgb advanced features
+
+#SRC += muse.c
