37cf718d6c3ca36fac77d3bbae9b6f55d31f0e4d
Bryan Ames <bryan@gantep.org>
2021-08-19 09:22:25 -0700

    moved space to accomodate gaming posture

--- keyboards/preonic/keymaps/amesb/keymap.c
+++ keyboards/preonic/keymaps/amesb/keymap.c
@@ -35,11 +35,11 @@ enum preonic_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_COLEMAK] = LAYOUT_preonic_grid(
-  KC_ESC,        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_PSCR,
-  KC_TAB,        KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_QUOT,
-  KC_CAPS,       KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_ENT,
-  OSM(MOD_LSFT), KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, OSM(MOD_RSFT),
-  KC_LCTL,       KC_DEL,  KC_LGUI, KC_LALT, KC_SPC,  LOWER,   KC_BSPC, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_RCTL
+  KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_PSCR,
+  KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_QUOT,
+  KC_CAPS, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_ENT,
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, OSM(MOD_RSFT),
+  KC_LCTL, KC_DEL,  KC_LGUI, KC_LALT, KC_SPC,  LOWER,   KC_BSPC, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, OSM(MOD_RCTL)
 ),
 
 [_QWERTY] = LAYOUT_preonic_grid(
